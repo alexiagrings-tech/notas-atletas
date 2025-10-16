7c306f551031736ef2997a679feab49ae56fc9ca
alexiagrings-tech <alexiagringsfurtado@gmail.com>
2025-10-16 16:14:52 -0300

    notas-atletas
    Código

---
+++ notas-atletas.js
@@ -0,0 +1,20 @@
+let atletas = [
+  { nome: "Cesar Abascal", notas: [10, 9.34, 8.42, 10, 7.88] },
+  { nome: "Fernando Puntel", notas: [8, 10, 10, 7, 9.33] },
+  { nome: "Daiane Jelinsky", notas: [7, 10, 9.5, 9.5, 8] },
+  { nome: "Bruno Castro", notas: [10, 10, 10, 9, 9.5] }
+];
+
+for (let i = 0; i < atletas.length; i++) {
+  const atleta = atletas[i];
+
+  let notas = atleta.notas.slice();
+  notas.sort((a, b) => a - b);
+
+  let notasComputadas = notas.slice(1, 4);
+
+  let soma = notasComputadas.reduce((acc, nota) => acc + nota, 0);
+
+  let media = soma / notasComputadas.length;
+
+console.log(`A média do atleta ${atleta.nome} é ${media}`);}
